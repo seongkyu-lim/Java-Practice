581097344cc18dbcc79ea5b53110b3c628bcfb93
seongkyu-lim <96x60812@naver.com>
2021-12-24 16:16:59 +0900

    add: inheritance

---
+++ src/Inheritance/CustomerTest.java
@@ -0,0 +1,20 @@
+package Inheritance;
+
+public class CustomerTest {
+
+    public static void main(String[] args) {
+        Customer lim = new Customer();
+        VIPCustomer kim = new VIPCustomer();
+
+        lim.setCustomerName("lim");
+        lim.setCustomerID(10010);
+        lim.bonusPoint = 1000;
+
+        kim.setCustomerName("kim");
+        kim.setCustomerID(10020);
+        kim.bonusPoint = 10000;
+
+        System.out.println(lim.showCustomerInfo());
+        System.out.println(kim.showCustomerInfo());
+    }
+}
---
+++ src/Inheritance/VIPCustomer.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class VIPCustomer extends Customer{
+
+    double saleRatio;
+    private int agentID;
+
+    public VIPCustomer(){
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        saleRatio = 0.1;
+    }
+}
