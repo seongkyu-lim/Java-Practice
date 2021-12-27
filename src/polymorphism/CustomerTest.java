60adc5a11130ec51abc37b978ec78ed94798bb83
seongkyu-lim <96x60812@naver.com>
2021-12-28 00:01:15 +0900

    add:practice

--- src/polymorphism/CustomerTest.java
+++ src/polymorphism/CustomerTest.java
@@ -5,6 +5,7 @@ public class CustomerTest {
     public static void main(String[] args) {
         Customer lim = new Customer();
         VIPCustomer kim = new VIPCustomer();
+        GoldCustomer lee = new GoldCustomer();
 
         lim.setCustomerName("lim");
         lim.setCustomerID(10010);
@@ -14,7 +15,16 @@ public class CustomerTest {
         kim.setCustomerID(10020);
         kim.bonusPoint = 10000;
 
+
+        lee.setCustomerName("lee");
+        lee.setCustomerID(100);
+        lee.bonusPoint = 5000;
+
+        System.out.println(lee.calcPrice(1000));
+
+
         System.out.println(lim.showCustomerInfo());
         System.out.println(kim.showCustomerInfo());
+        System.out.println(lee.showCustomerInfo());
     }
 }
